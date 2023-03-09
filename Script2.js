12af1d9e69a4dff0a256159fd4a943dcc6ecad38
polloeloy <42683251+polloeloy@users.noreply.github.com>
2023-03-08 23:26:03 -0300

    ddd

---
+++ Script2.js
@@ -0,0 +1,7 @@
+
+var este = document.getElementById("este");
+este.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
